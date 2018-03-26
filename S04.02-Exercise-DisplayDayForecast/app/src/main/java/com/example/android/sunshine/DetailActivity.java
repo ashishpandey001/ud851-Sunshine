b03e8249b2b1e2b2b13b9f659b64be86aa59f1ee
Ashish Pandey <ashishpandeyone@gmail.com>
2018-03-26 13:13:05 +0530

    completed S04.02-Exercise-DisplayDayForecast

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -2,7 +2,10 @@ package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -24,5 +27,26 @@ public class DetailActivity extends AppCompatActivity {
             weatherData = parentIntent.getStringExtra("weatherData");
             weatherForDay.setText(weatherData);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.share_weather_data) {
+            String dialogTitle = "Share Weather Details";
+            String mimeType = "text/plain";
+            ShareCompat.IntentBuilder
+                    .from(this)
+                    .setChooserTitle(dialogTitle)
+                    .setType(mimeType)
+                    .setText(weatherData)
+                    .startChooser();
+        }
+        return true;
     }
 }
